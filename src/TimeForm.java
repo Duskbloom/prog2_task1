d2ffac7dbf672fa5becfe5f74d35cc6e697c2c84
Hannes Hornwall <hannes@rabble.se>
2014-04-01 15:40:58 +0200

    Added TimeForm.

---
+++ src/TimeForm.java
@@ -0,0 +1,59 @@
+import javax.swing.*;
+
+class TimeForm extends JPanel {
+  private JTextField startNumberField;
+  private JTextField timeField;
+
+  public TimeForm(){
+    setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+
+    add(createNumberPanel());
+    add(createTimePanel());
+  }
+
+  private JPanel createNumberPanel(){
+    startNumberField = new JTextField(3);
+    JLabel startNumberLabel = new JLabel("Startnr: ");
+    JPanel panel = new JPanel();
+    panel.add(startNumberLabel);
+    panel.add(startNumberField);
+
+    return panel;
+  }
+
+  private JPanel createTimePanel(){
+    timeField = new JTextField(5);
+    JLabel timeLabel = new JLabel("Tid: ");
+    JPanel panel = new JPanel();
+    panel.add(timeLabel);
+    panel.add(timeField);
+
+    return panel;
+  }
+
+  public int getStartNumber(){
+    return Integer.parseInt(startNumberField.getText());
+  }
+
+  public double getTime(){
+    return Double.parseDouble(timeField.getText());
+  }
+  
+  private boolean isValidInteger(String input){
+    try{
+      Integer.parseInt(input);
+    }catch(NumberFormatException e){
+      return false;
+    }
+    return true;
+  }
+  
+  private boolean isValidDouble(String input){
+    try{
+      Double.parseDouble(input);
+    }catch(NumberFormatException e){
+      return false;
+    }
+    return true;
+  }
+}
