9ba440b3b90a0754c70f1bd568b5c70b200982bc
Sofie Andersson <sofie1.andersson@gmail.com>
2014-04-01 17:09:47 +0200

    Ändrade testobjekt för bättre sortering

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,8 @@ import java.util.ArrayList;
 public class Main{
   public static void main (String[]args){
     Runner r = new Runner("Ola", "Sverige", 43, 354);
-    Runner d = new Runner("Eli", "Kanada", 25, 34);
-    Runner b = new Runner("Ken", "USA", 37, 63);
+    Runner d = new Runner("Simon", "Kanada", 25, 34);
+    Runner b = new Runner("Ken", "USA", 94, 63);
     ArrayList<Runner> Runners = new ArrayList<Runner>();
     Runners.add(r);
     Runners.add(d);
